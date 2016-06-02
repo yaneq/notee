1f341e97eeebcc645a3c90bc4e0865db7d609b1e
Jan Koeppen <jan@yaneq.com>
2016-06-02 21:59:03 +0200

    tag directive recognizes dashes

--- public/javascripts/directives/tagged-text.js
+++ public/javascripts/directives/tagged-text.js
@@ -1,14 +1,14 @@
 var module = angular.module('notee.directives', ['ngSanitize']);
-module.directive('taggedText', function() {
+module.directive('taggedText', function () {
   return {
     restrict: 'E',
     scope: {
       text: '='
     },
     template: '<note ng-bind-html="format(text)"/>}',
-    link: function(scope, element, attrs, controllers) {
-      scope.format = function(text) {
-        return text.replace(/(#\w+)/, '<span class="tag">$1</span>');
+    link: function (scope, element, attrs, controllers) {
+      scope.format = function (text) {
+        return text.replace(/(#\S+)/, '<span class="tag">$1</span>');
       }
     }
   }
